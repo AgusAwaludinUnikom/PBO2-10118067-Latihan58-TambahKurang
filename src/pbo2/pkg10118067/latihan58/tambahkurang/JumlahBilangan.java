59cad131d593a8ac0a5942c851662ca24f4823de
Agus Awaludin <55693286+AgusAwaludinUnikom@users.noreply.github.com>
2019-11-07 18:47:19 +0700

    Add files via upload

---
+++ src/pbo2/pkg10118067/latihan58/tambahkurang/JumlahBilangan.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.pkg10118067.latihan58.tambahkurang;
+
+/**
+ *
+ * @author Kiyo
+ */
+public class JumlahBilangan extends Bilangan{
+    
+    public void tampilHasilJumlah(){
+        System.out.println("Hasil perjumlahan = " + (getX() + getY()));
+    }
+}
---
+++ src/pbo2/pkg10118067/latihan58/tambahkurang/PBO210118067Latihan58TambahKurang.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.pkg10118067.latihan58.tambahkurang;
+
+/**
+ *
+ * @author
+ * NAMA : Agus Awaludin
+ * NIM : 10118067
+ * KELAS : PBO2
+ * Latihan ini berisi tentang cara menampilkan penjumlahan dan pengurangan
+ * dengan metode polymorpism
+ */
+public class PBO210118067Latihan58TambahKurang {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        JumlahBilangan jumlah = new JumlahBilangan();
+        jumlah.tampilHasilJumlah();
+        
+        SelisihBilangan selisih = new SelisihBilangan();
+        selisih.tampilSelisih();
+    }
+    
+}
